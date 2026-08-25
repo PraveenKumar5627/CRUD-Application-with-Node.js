901f73417b7bb4bef94de540515457a9be484ee5
PraveenKumar5627 <praveen80280@gmail.com>
2026-08-25 21:02:02 +0530

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,22 @@ import axios from "axios"
 function App() {
   const [users, setUsers] = useState([])
   const [filterUsers, setFilterUser] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [userData, setUserData] = useState({ name: "", age: "", city: "" })
+  function handleAddRecords() {
+    setIsModalOpen(true)
+    console.log("modal")
+    setUserData({name:"",age:"",city:""})
+  }
+
+  function closeModal() {
+    setIsModalOpen(false)
+    getAllUsers()
+  }
+  function handleData(e) {
+    setUserData({ ...userData, [e.target.name]: e.target.value })
+  }
+ 
   const getAllUsers = async () => {
     await axios.get(`http://localhost:8000/users`).then
       ((res) => {
@@ -27,14 +43,33 @@ function App() {
   const handleDelete = async (id) => {
     const isconfirmed = window.confirm("Are you sure want to delter this user..")
     if (isconfirmed) {
-      const res = await axios.delete(`http://localhost:8000/users/:${id}`)
+      const res = await axios.delete(`http://localhost:8000/users/${id}`)
       setUsers(res.data)
       setFilterUser(res.data)
     }
-    else{
+    else {
       console.log("some erre")
     }
 
+
+  }
+  async function handleOver(e) {
+    e.preventDefault()
+    if (userData.id) {
+      await axios.patch(`http://localhost:8000/users/${userData.id}`, userData).then((res) => {
+        console.log(res)
+      })
+    } else{
+      await axios.post(`http://localhost:8000/users/`, userData).then((res) => {
+      console.log(res)
+    })
+  }
+
+  closeModal()
+  }
+   function updateRecord(user) {
+    setUserData(user)
+    setIsModalOpen(true)
   }
 
   return (
@@ -45,7 +80,7 @@ function App() {
         </h3>
         <div className="input-search">
           <input type="text" placeholder='Search Here..Dude' onChange={handleSearch} />
-          <button className='btn green'>Add Record</button></div>
+          <button className='btn green' onClick={handleAddRecords}>Add Record</button></div>
         <table className='table'>
           <thead>
             <tr>
@@ -64,11 +99,29 @@ function App() {
                 <td>{user.name}</td>
                 <td>{user.age}</td>
                 <td>{user.city}</td>
-                <td> <button className='btn green'>Edit</button></td>
+                <td> <button className='btn green' onClick={() => updateRecord(user)}>Edit</button></td>
                 <td ><button className='btn red' onClick={() => handleDelete(user.id)}>Delete</button></td>
               </tr>)}
           </tbody>
         </table>
+        {isModalOpen && (<div className='modal'>
+          <div className="modal-content">
+            <span className='close' onClick={closeModal}>&times;</span>
+           <h2>{userData.id?"Update Record":"Add Record"}</h2>
+            <div className="input-group">
+              <label htmlFor="name">Full Name:</label>
+              <input type="text" placeholder='Enter a Name:' name='name' onChange={(e) => handleData(e)} value={userData.name} /></div>
+            <div className="input-group">
+              <label htmlFor="age">Age:</label>
+              <input type="number" placeholder='Enter a Age:' name='age' onChange={(e) => handleData(e)}  value={userData.age}/></div>
+            <div className="input-group">
+              <label htmlFor="city">City:</label>
+              <input type="text" placeholder='Enter a City:' name='city'  value={userData.city}
+                onChange={(e) => handleData(e)} /></div>
+            <button className='btn green' onClick={handleOver}>{userData.id?"Update Record":"Add Record"}</button>
+          </div>
+        </div>
+        )}
       </div>
     </>
   )
--- server/index.js
+++ server/index.js
@@ -1,36 +1,104 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require("fs")
-const app = express()
-app.use(cors());
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const app = express();
+app.use(express.json())
 const port = 8000;
-let users = require("./Sample.json");
-const { error } = require('console');
-const { type } = require('os');
-// const { error } = require('console');
 
+app.use(cors());
+
+let users = require("./Sample.json");
+
+// GET all users
 app.get("/users", (req, res) => {
-    return res.json(users)
-})
+    return res.json(users);
+});
+
+// DELETE user
 app.delete("/users/:id", (req, res) => {
-    console.log("id rceived",req.params.id)
-    var index = users.filter(user=>user.id==4)
-    console.log("index",index)
-    if(index===-1){
+
+    console.log("Raw ID received:", req.params.id);
+
+    const id = Number(req.params.id);
+
+    console.log("Converted ID:", id);
+
+    const index = users.findIndex(
+        user => Number(user.id) === id
+    );
+
+    console.log("Index:", index);
+
+    if (index === -1) {
         return res.status(404).json({
-            error:"user is not fouund"
-        })
+            error: "User not found"
+        });
     }
-    users.splice(req.params.id,1)
-    fs.writeFile("./Sample.json", JSON.stringify(users, null, 2), (err) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).json({ error:"failed to delter user" })
+
+    users.splice(index, 1);
+
+    fs.writeFile(
+        "./Sample.json",
+        JSON.stringify(users, null, 2),
+        (err) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    error: "Failed to delete user"
+                });
+            }
+            return res.json(users);
         }
-        console.log("delete request rceied")
-        return res.json(users)
-    })
+    );
+});
+app.post("/users", (req, res) => {
+    let { name, age, city } = req.body
+    if (!name || !age || !city) {
+        return res.status(404).send("all field required")
+    }
+    let id = Date.now()
+    users.push({ id, name, age, city })
+    fs.writeFile(
+        "./Sample.json",
+        JSON.stringify(users, null, 2),
+        (err) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    error: "Failed to delete user"
+                });
+            }
+            return res.json({ message: "data received completely" })
+        }
+    );
+
 })
-app.listen(port, (err) => {
-    console.log(`app is running in port ${port}`)
+app.patch("/users/:id", (req, res) => {
+    let id = Number(req.params.id)
+    let { name, age, city } = req.body
+    if (!name || !age || !city) {
+        return res.status(404).send("all field required")
+    }
+    let index = users.findIndex((user) => user.id == id)
+    users.splice(index, 1, { ...req.body })
+    fs.writeFile(
+        "./Sample.json",
+        JSON.stringify(users, null, 2),
+        (err) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    error: "Failed to delete user"
+                });
+            }
+            return res.json({ message: "data received completely" })
+        }
+    );
+
 })
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
